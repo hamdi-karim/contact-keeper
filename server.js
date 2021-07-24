bf6fd5c587c71aab7d1523fd0d07d277a7d70bfa
Karim <karim@datenexperten.com>
2021-07-25 01:09:05 +0200

    user model and create user request paramaters validation

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // connect Database
 connectDB();
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Keeper API' })
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact Keeper API" })
 );
 
 // Define Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/users', require('./routes/users'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
