046e36fb14e6bc6886150771dc212614e58e05f3
Karim <karim@datenexperten.com>
2021-09-24 10:40:21 +0200

    Update contacts API integration

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,6 +64,29 @@ const ContactState = (props) => {
     }
   };
 
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACTS, payload: res.data });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+
   // Delete Contact
   const deleteContact = async (id) => {
     try {
@@ -92,11 +115,6 @@ const ContactState = (props) => {
   // Clear current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACTS, payload: contact });
   };
 
   // Filter Contacts
